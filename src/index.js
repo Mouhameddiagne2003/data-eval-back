2241c4f046471aa4b78beb2583345f1f19ae3386
Mouhameddiagne2003 <mouhameddiagne029@gmail.com>
2025-03-24 20:03:45 +0000

    refractor : add origin ( url in production ) in socket conf ( cors problem )

--- src/index.js
+++ src/index.js
@@ -19,8 +19,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", // À ajuster selon le domaine du front
-        methods: ["GET", "POST"]
+        origin: "https://data-eval-frontend.onrender.com", // À ajuster selon le domaine du front
+        methods: ["GET", "POST"],
+        credentials: true,
     }
 });
 const PORT = process.env.PORT || 5000;
